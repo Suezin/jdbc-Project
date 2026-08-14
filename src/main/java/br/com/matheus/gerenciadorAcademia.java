ef895355445aff3bea82941dfd023ab0a0250892
Suezin <matheusbaumhardt654@gmail.com>
2026-08-14 13:55:09 -0300

    New Feature: Added a terminal form to use program

--- src/main/java/br/com/matheus/gerenciadorAcademia.java
+++ src/main/java/br/com/matheus/gerenciadorAcademia.java
@@ -50,12 +50,74 @@ public class gerenciadorAcademia {
                     System.out.println(" 5. Sair");
                     System.out.println(" Informe a sua opção :");
                     j = sc.nextInt();
-                }
-                if (j == 1){
-                    List<Aluno> alunoList = new ArrayList<>();
-                    alunoList = conn.getAluno();
-                    for(Aluno aluno : alunoList){
+                    if (j == 1){
+                        List<Aluno> alunoList =conn.getAluno();
+                        for(Aluno aluno : alunoList){
+                            System.out.println("================================");
+                            System.out.println("Nome: " + aluno.getNome());
+                            System.out.println("Data de Nascimento : " + aluno.getDataNasc());
+                            System.out.println("Cpf : " + aluno.getCpf());
+                            System.out.println("Tipo de Plano : " + aluno.getTipoDePlano());
+                            if (aluno.getPagamentoPlano() == false)
+                                System.out.println("Pagamento do Plano: Não pago");
+                            System.out.println("Pagamento do Plano : Pago");
+                            System.out.println("Treino : " + aluno.getIdTreino());
+                            System.out.println("================================");
+
+                        }
+                        System.out.println(" 0. Sair");
+                        int sair = 0;
+                        sc.nextInt();
+                        if (sair>= 0) {
+                            j = 0;
+                        }
+
+                    }else if(j == 2){
+                        String nome = "";
+                        int diaNasc = 0;
+                        int mesNasc = 0;
+                        int anoNasc = 0;
+                        String cpf = "";
+                        String plano = "";
+                        LocalDate dataNasc;
+                        int idTreino = 0;
+                        sc.nextLine();
+
+                        System.out.println("================================");
+                        System.out.println("          Inserir Aluno         ");
+                        System.out.println("Informe o nome: ");
+                        nome = sc.nextLine();
+
+                        System.out.println("Informe a data de Nascimento:");
+                        System.out.println("Dia : ");
+                        diaNasc = sc.nextInt();
+                        System.out.println("Mês : ");
+                        mesNasc = sc.nextInt();
+                        System.out.println("Ano : ");
+                        anoNasc = sc.nextInt();
+                        dataNasc = LocalDate.of(anoNasc,mesNasc,diaNasc);
+
+                        sc.nextLine();
+                        System.out.println("Informe o cpf :");
+                        cpf = sc.nextLine();
+                        if (cpf.length()!= 11 ) {
+                            System.out.println("Numero de cpf Inválido!");
+                            System.out.println("Informe o cpf : ");
+                            cpf = sc.nextLine();
+                        }
+
+                        System.out.println("Informe o tipo de plano que deseja (Ex: Mensal, trimestral, etc) : ");
+                        plano = sc.nextLine();
+                        sc.nextLine();
+                        Boolean pagamento = true;
+
+                        System.out.println("Informe o tipo de Treino de preferência: ");
+                        idTreino = sc.nextInt();
+
+                        conn.insertAluno(nome,dataNasc,cpf,plano,pagamento,idTreino);
+                        System.out.println("Aluno Inserido com sucesso !" );
                     }
+
                 }
             }
         }
